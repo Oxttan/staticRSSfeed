5c1478a0d1723d0dda53a930d2c16ce4046002c5
Oxttan <148010423+Oxttan@users.noreply.github.com>
2023-10-15 22:08:23 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const apiKey = 'ftukbsji3qqrpl4nwiftgmsh7c2inufrg1fabpi1';
+const apiKey = 'hvr6eeydxjtsdu93bxsbpmxr4gbte9dtt8khhmdl';
 const apiUrl = `https://api.rss2json.com/v1/api.json?rss_url=&api_key=${apiKey}`;
 const jsonConfigUrl = 'config.json';
 const keywordsUrl = 'keywords.json';
